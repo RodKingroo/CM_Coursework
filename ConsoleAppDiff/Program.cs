c1a3169bed690093c250f1bf3f04f5e8829a4696
RodKingroo <hot.hunter@mail.ru>
2022-05-20 14:37:26 +0600

    Newton

--- ConsoleAppDiff/Program.cs
+++ ConsoleAppDiff/Program.cs
@@ -1,35 +1,40 @@
 ﻿namespace ConsoleAppDiff
 {
-    class Program{
-        static void Main(string[] args){
-            
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
             Console.WriteLine("Дано: \tf(x) = sin(1.8x) \n" +
                 "\txпринадлежащей[0, PI]. \n" +
                 "\tx |\t0 |\tPI/4 |\tPI/2 |\t3PI/4 |\tPI |\n" +
                 "\t----------------------------------------------\n" +
                 "\ty | \t  | \t     | \t     | \t      |    | \n");
-            
+
             List<double> x = new List<double>();
             List<double> fy = new List<double>();
             List<double> daprF = new List<double>();
             List<double> dsimF = new List<double>();
             List<double> dLgrF = new List<double>();
+            List<double> dNwtF = new List<double>();
 
 
             double dx = Math.PI / 4;
-            
+
             x.Add(0);
-            
-            for (int i = 0; x[i] <= Math.PI; i++) {
+
+            for (int i = 0; x[i] <= Math.PI; i++)
+            {
                 fy.Add(Math.Sin(1.8 * x[i]));
-                Console.WriteLine($" f(x[{i}]) = f({Math.Round(x[i], 3)}) = {Math.Round(fy[i],3)}");
+                Console.WriteLine($" f(x[{i}]) = f({Math.Round(x[i], 3)}) = {Math.Round(fy[i], 3)}");
                 x.Add(x[i] + dx);
             }
 
             Console.WriteLine("\tДифференцирование функций, используя симметричную формулу");
-            for (int i = 1; x[i] <= Math.PI; i++){
+            for (int i = 1; x[i] <= Math.PI; i++)
+            {
                 dsimF.Add(simetrical(x[i], dx));
-                Console.WriteLine($"df(x[{i}]) = f({Math.Round(x[i], 3)}) = {Math.Round(dsimF[i-1],3)}");
+                Console.WriteLine($"df(x[{i}]) = f({Math.Round(x[i], 3)}) = {Math.Round(dsimF[i - 1], 3)}");
             }
 
             Console.WriteLine("\tДифференцирование функции, используя приближеную формулу");
@@ -46,11 +51,18 @@
                 Console.WriteLine($"df(x[{i}]) = f({Math.Round(x[i], 3)}) = {Math.Round(dLgrF[i - 1], 3)}");
             }
 
+            Console.WriteLine("\tДифференцирование функции через построение интерполяции Ньютона");
+            for (int i = 1; x[i] <= Math.PI; i++)
+            {
+                dNwtF.Add(Newton(x, fy, x[i], dx));
+                Console.WriteLine($"df(x[{i}]) = f({Math.Round(x[i], 3)}) = {Math.Round(dNwtF[i - 1], 3)}");
+            }
+
             while (Console.ReadKey().Key != ConsoleKey.Escape) ;
         }
 
         /*f(x) = sin(1.8*x)*/
-        static double f(double x){ return Math.Sin(1.8 * x); }
+        static double f(double x) { return Math.Sin(1.8 * x); }
 
         static double approx(double x, double h)
         {
@@ -62,37 +74,65 @@
             return (f(x + h) - f(x - h)) / (2 * h);
         }
 
-        static double Lagrange(List <double> x, List <double> y, double p)
+        static double Lagrange(List<double> x, List<double> y, double p)
         {
             double yy = 0;
             double n = x.Count - 2;
             double t1;
             double t2;
             List<double> L = new List<double>();
-            
+
             for (int i = 0; i < n; i++)
             {
                 t1 = 1;
                 t2 = 1;
                 for (int j = 0; j < n; j++)
                 {
-                    if (i != j) {
+                    if (i != j)
+                    {
                         t1 *= (p - x[j]);
                         t2 *= (x[i] - x[j]);
-                        
                     }
                 }
-
                 L.Add(y[i] * (t1 / t2));
             }
-
-            for(int i = 0; i < L.Count; i++)
+            for (int i = 0; i < L.Count; i++)
             {
                 yy += L[i];
             }
-
             return yy;
 
+        }
+
+        static double Newton(List<double> x, List<double> y, double p, double h)
+        {
+            double yy = 0;
+            double c = 0;
+            int n = x.Count - 2;
+
+            List<double> dy = new List<double>();
+
+            for (int i = 0; i < n; i++){
+                dy.Add(y[i]);
+            }
+            for (int j = 1; j < n; j++)
+            {
+                for(int jj = n; jj < j; jj--)
+                {
+                    dy[jj] = dy[jj] - dy[jj - 1];
+                }
+            }
+            yy = y[0];
+            c = (p - x[0]) / h;
+            for (int ii = 1; ii < n; ii++)
+            {
+                yy += dy[ii] * c;
+                c *= (p - x[ii]) / (h * n);
+            }
+
+            double sum = 0;
+            sum += c+yy;
+            return sum;
         }
     }
 }
